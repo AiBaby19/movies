351314a7c56f036abe7b42bfe347a957d10154cb
dvash999 <myselfandme@gmail.com>
2019-02-04 19:02:40 +0200

    init commit

---
+++ client/src/search/SearchBar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './searchBar.css'
+
+
+class SearchBar extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <React.Fragment>
+        <div className="container">
+        
+        </div>
+      </React.Fragment>
+     );
+  }
+}
+ 
+export default SearchBar;
