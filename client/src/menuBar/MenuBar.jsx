807f8c1385fc0f596c61ee0978b87d7f5548c878
dvash999 <myselfandme@gmail.com>
2019-02-04 20:29:15 +0200

    first layout

---
+++ client/src/menuBar/MenuBar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './menuBar.css';
+
+
+class MenuBar extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <React.Fragment>
+        <div className="container-menu">
+      
+        </div>
+      </React.Fragment>
+     );
+  }
+}
+ 
+export default MenuBar;
--- client/src/search/SearchBar.jsx
+++ client/src/search/SearchBar.jsx
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import './searchBar.css'
-
+import React, {Component} from 'react';
+import './searchBar.css';
 
 class SearchBar extends Component {
-  state = {  }
-  render() { 
-    return ( 
-      <React.Fragment>
-        <div className="container">
-        
-        </div>
-      </React.Fragment>
-     );
-  }
+    state = {}
+    render() {
+        return (
+            <React.Fragment>
+                <div className="container">
+                    <div className="wrapper">
+                        <input className="searchInput" type="text" placeholder="Search A Movie..."/>
+                        <div className="filter">
+                          FILTER
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
- 
+
 export default SearchBar;
