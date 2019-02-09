dee2fe4c154f5da8ff2492a577ccbfef16fbcba5
dvash999 <myselfandme@gmail.com>
2019-02-09 21:52:35 +0200

    Updates

---
+++ precache-manifest.1abaeeac5bbda4c1f9a2c2c652e9281c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc8b9d642e0b86e56ba3b1b2f363fb78",
+    "url": "/movies/static/media/jam_type_design_-_toma_sans_regular-webfont.cc8b9d64.ttf"
+  },
+  {
+    "revision": "d53c70e9a4abf90dd0e7ca2d01dee0a6",
+    "url": "/movies/static/media/PhosphatePro-Inline.d53c70e9.otf"
+  },
+  {
+    "revision": "6ad4d8afb5a54c964d9f",
+    "url": "/movies/static/js/runtime~main.6ad4d8af.js"
+  },
+  {
+    "revision": "b77da97e56fea91f2622",
+    "url": "/movies/static/js/main.b77da97e.chunk.js"
+  },
+  {
+    "revision": "c631ba940dc9451f8d0b",
+    "url": "/movies/static/js/1.c631ba94.chunk.js"
+  },
+  {
+    "revision": "b77da97e56fea91f2622",
+    "url": "/movies/static/css/main.97c54605.chunk.css"
+  },
+  {
+    "revision": "b078e94c62fc7eb9e3f72b8eee1ccd78",
+    "url": "/movies/index.html"
+  }
+];
